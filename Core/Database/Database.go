f2950ca530c389fa5f5a816a6ececf6cafab03bb
DavidsPrusis <davidsprusis03@gmail.com>
2022-06-12 21:52:34 +0300

    Commit database dir

---
+++ Core/Database/Database.go
@@ -0,0 +1,36 @@
+package Database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq" // <------------ here
+	"log"
+)
+
+var db *sql.DB
+
+func GetConnection() *sql.DB {
+	return db
+}
+
+func ConnectDatabase() {
+	// Capture connection properties.
+	user := "postgres"
+	password := "........"
+	host := "127.0.0.1"
+	port := "5432"
+	dbname := "shortener"
+
+	// Get a database handle.
+	var err error
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	fmt.Println(psqlInfo)
+	db, err = sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatalf("DB Error : %s", err)
+	}
+
+	fmt.Println("Database Connected!")
+}
